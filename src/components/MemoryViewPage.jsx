e2e5ed30bd0d05e55dc70ca7b3406b4d6c81e9c5
Nison Malayev <nmalayev@gmail.com>
2019-03-18 18:34:35 -0400

    added search functionality to all memories page

--- src/components/MemoryViewPage.jsx
+++ src/components/MemoryViewPage.jsx
@@ -5,8 +5,19 @@ import '../css/MemoryViewPage.css';
 import { Card } from 'semantic-ui-react';
 
 function MemoryViewPage(props) {
+  const { searchQuery, memories } = props;
   const renderMemoryCards = () => {
-    return props.memories.map(mem => {
+    if (searchQuery) {
+      return memories
+        .filter(mem =>
+          mem.description.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+        .map(mem => {
+          return <MemoryCard key={mem.id} memory={mem} />;
+        });
+    }
+
+    return memories.map(mem => {
       return <MemoryCard key={mem.id} memory={mem} />;
     });
   };
