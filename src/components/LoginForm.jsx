45dc1b64ec6f27d5bd89c89be7ae32c4f73a94e6
Nison Malayev <nmalayev@gmail.com>
2019-03-20 23:10:45 -0400

    login backend works

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Button, Form } from 'semantic-ui-react';
+
+class LoginForm extends Component {
+  state = {
+    username: '',
+    password: ''
+  };
+
+  handleChange = (e, { name, value }) =>
+    this.setState({ [name]: value }, () => console.log(this.state));
+
+  handleSubmit = () => {
+    const { username, password } = this.state;
+
+    fetch('http://localhost:3001/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    }) // end of fetch
+      .then(r => r.json())
+      .then(response => {
+        if (response.errors) {
+          alert(response.errors);
+        } else {
+          this.props.history.push('/timeline');
+        }
+      });
+  };
+
+  render() {
+    const { username, password } = this.state;
+
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Input
+            fluid
+            label='Username'
+            type='text'
+            placeholder='Username'
+            name='username'
+            onChange={this.handleChange}
+            value={username}
+          />
+          <Form.Input
+            fluid
+            label='Password'
+            type='password'
+            placeholder='Password'
+            name='password'
+            onChange={this.handleChange}
+            value={password}
+          />
+          <Button
+            type='submit'
+            content='Login'
+            disabled={username && password ? false : true}
+          />
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
