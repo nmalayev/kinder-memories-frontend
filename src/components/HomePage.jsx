456d6ae7d33b64ccfc34b7abf0504f8c3051108a
Nison Malayev <nmalayev@gmail.com>
2019-03-22 14:33:40 -0400

    background video working

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
+import backgroundVideo from '../baby_stock_video.mp4';
 
 function HomePage() {
   return (
     <div>
-      <h2>Home Page</h2>
-      <iframe
-        width='560'
-        height='315'
-        src='https://www.youtube.com/embed/FlNHeNiz_PU?autoplay=1&loop=1'
-        frameborder='0'
-        allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-        allowfullscreen
-      />
+      <video loop autoplay='autoplay'>
+        <source src={backgroundVideo} type='video/mp4' />
+        <source src={backgroundVideo} type='video/ogg' />
+      </video>
     </div>
   );
 }
