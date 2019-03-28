eed422ddc4ac3f1dfabe06ba704b08b529130380
Nison Malayev <nmalayev@gmail.com>
2019-03-28 12:08:30 -0400

    changed font and colors on new form

--- src/components/MemoryForm.jsx
+++ src/components/MemoryForm.jsx
@@ -34,82 +34,80 @@ class MemoryForm extends Component {
 
   render() {
     return (
-      <Modal open={this.props.showAddModal}>
-        <Modal.Header>New Memory</Modal.Header>
+      <Modal open={this.props.showAddModal} size='tiny' id='modal-form'>
+        <Modal.Header id='modal-header'>
+          Add New Memory to Timeline
+        </Modal.Header>
         <Modal.Content>
-          <Modal.Description>
-            <Header>Add New Memory</Header>
-            <Button onClick={() => this.props.history.goBack()}>Go Back</Button>
+          <Form onSubmit={this.props.handleSubmit} id='new-memory-form'>
+            <Form.Select
+              fluid
+              selection
+              label='Memory Type'
+              color='black'
+              name='newMemType'
+              options={options}
+              placeholder='Type'
+              onChange={this.props.handleSelectChange}
+              value={this.props.newMemType}
+              required
+            />
+            <Form.Input
+              label='Date of Memory'
+              type='date'
+              name='newMemDate'
+              min='2018-04-13'
+              onChange={this.props.handleChange}
+              required
+            />
+            <Form.Input
+              fluid
+              label='Title'
+              placeholder='Title'
+              name='newMemTitle'
+              onChange={this.props.handleChange}
+              required
+            />
+            <Form.TextArea
+              label='Description'
+              placeholder={`Tell us more about your memory of ${
+                this.props.childName
+              }...`}
+              name='newMemDescription'
+              onChange={this.props.handleChange}
+              required
+            />
 
-            <Form onSubmit={this.props.handleSubmit} id='new-memory-form'>
-              {/* <Form.Group widths='equal'> */}
-              <Form.Select
-                fluid
-                selection
-                label='Memory Type'
-                color='black'
-                name='newMemType'
-                options={options}
-                placeholder='Type'
-                onChange={this.props.handleSelectChange}
-                value={this.props.newMemType}
-                required
-              />
+            {this.props.newMemType === 'photo' ||
+            this.props.newMemType === 'video' ? (
               <Form.Input
-                label='Date of Memory'
-                type='date'
-                name='newMemDate'
-                min='2018-04-13'
-                onChange={this.props.handleChange}
+                label={`Attach ${this.props.newMemType}`}
+                type='file'
+                name='file'
+                accept={
+                  this.props.newMemType === 'photo' ? 'image/*' : 'video/*'
+                }
                 required
               />
-              <Form.Input
-                fluid
-                label='Title'
-                placeholder='Title'
-                name='newMemTitle'
-                onChange={this.props.handleChange}
-                required
-              />
+            ) : null}
+            {this.props.newMemType === 'letter' ? (
               <Form.TextArea
-                label='Description'
-                placeholder={`Tell us more about your memory of ${
-                  this.props.childName
-                }...`}
-                name='newMemDescription'
+                label='Letter (max 500 characters)'
+                placeholder={`Write a note for ${this.props.childName}...`}
+                name='newMemLetter'
+                maxlength='500'
                 onChange={this.props.handleChange}
+                id='newMemLetter'
                 required
               />
-
-              {this.props.newMemType === 'photo' ||
-              this.props.newMemType === 'video' ? (
-                <Form.Input
-                  label={`Attach ${this.props.newMemType}`}
-                  type='file'
-                  name='file'
-                  accept={
-                    this.props.newMemType === 'photo' ? 'image/*' : 'video/*'
-                  }
-                  required
-                />
-              ) : null}
-              {this.props.newMemType === 'letter' ? (
-                <Form.TextArea
-                  label='Letter (max 500 characters)'
-                  placeholder={`Write a note for ${this.props.childName}...`}
-                  name='newMemLetter'
-                  maxlength='500'
-                  onChange={this.props.handleChange}
-                  id='newMemLetter'
-                  required
-                />
-              ) : null}
-              {/* </Form.Group> */}
-              <Form.Group>
-                <Form.Button>Submit</Form.Button>
-              </Form.Group>
-            </Form>
-          </Modal.Description>
+            ) : null}
+            <Form.Group id='new-form-buttons'>
+              <Button negative onClick={() => this.props.history.goBack()}>
+                Go Back
+              </Button>
+              <Form.Button positive>Submit</Form.Button>
+            </Form.Group>
+          </Form>
         </Modal.Content>
       </Modal>
     );
